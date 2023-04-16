77279a462f0545793700c62613caf6a24dfdadb2
bobur2006coder <boburdilmurodov702@gmail.com>
2023-04-16 05:36:58 +0500

    fix

--- js.js
+++ js.js
@@ -2,19 +2,19 @@ let str = prompt("Harfni kiriting");
 
 
     if(str == "a"){
-        console.log(str + " bu harf unli");
+        alert(str + " bu harf unli");
     }else if (str=="e"){
-        console.log(str + " bu harf unli");
+        alert(str + " bu harf unli");
     }else if (str=="i"){
-        console.log(str + " bu harf unli");
+        alert(str + " bu harf unli");
     }else if (str=="o"){
-        console.log(str + " bu harf unli");
+        alert(str + " bu harf unli");
     }else if (str=="u"){
-        console.log(str + " bu harf unli");
+        alert(str + " bu harf unli");
     }else if (str=="o`"){
-        console.log(str + " bu harf unli");
+        alert(str + " bu harf unli");
     }else{
-        console.log(str + " bu harf undosh");
+        alert(str + " bu harf undosh");
     }
 
 
