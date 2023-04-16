a35eda0a833b44e56a5a00a58c75198f78c1d4e5
bobur2006coder <boburdilmurodov702@gmail.com>
2023-04-16 05:33:25 +0500

    fix

---
+++ js.js
@@ -0,0 +1,31 @@
+let str = prompt("Harfni kiriting");
+
+
+    if(str == "a"){
+        console.log(str + " bu harf unli");
+    }else if (str=="e"){
+        console.log(str + " bu harf unli");
+    }else if (str=="i"){
+        console.log(str + " bu harf unli");
+    }else if (str=="o"){
+        console.log(str + " bu harf unli");
+    }else if (str=="u"){
+        console.log(str + " bu harf unli");
+    }else if (str=="o`"){
+        console.log(str + " bu harf unli");
+    }else{
+        console.log(str + " bu harf undosh");
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
